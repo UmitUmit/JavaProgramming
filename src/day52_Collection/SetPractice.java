2cbfd7988c64a857595414209cb09e716f46bdfc
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-01-29 15:21:08 -0500

    Day52 Class

---
+++ src/day52_Collection/SetPractice.java
@@ -0,0 +1,24 @@
+package day52_Collection;
+
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class SetPractice {
+
+    public static void main(String[] args) {
+
+        Set<String> names1 = new HashSet<>();
+
+
+        Set<String> names2 = new LinkedHashSet<>();
+
+
+        Set<String> names3 = new TreeSet<>();
+
+
+
+    }
+
+}
