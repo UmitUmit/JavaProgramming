fefe684333a88e4a7b6a1b1204b0ee12c40428da
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-02-01 15:05:28 -0500

    Day53 Class

---
+++ src/day53_Collection/FrequencyOfCharacters2.java
@@ -0,0 +1,15 @@
+package day53_Collection;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class FrequencyOfCharacters2 {
+
+    public static void main(String[] args) {
+        String str = "aaabbc";
+        Map<String, Integer> result = new LinkedHashMap<>();
+
+
+
+    }
+}
