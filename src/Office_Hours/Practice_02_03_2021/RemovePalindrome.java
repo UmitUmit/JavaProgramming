0f2c2cf26de80ba62b0293a00fe8b221658423f2
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-02-03 15:37:19 -0500

    Office Hour: 02/03/2021

---
+++ src/Office_Hours/Practice_02_03_2021/RemovePalindrome.java
@@ -0,0 +1,40 @@
+package Office_Hours.Practice_02_03_2021;
+
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+
+public class RemovePalindrome {
+
+    public static void main(String[] args) {
+        String[] words = {"Level", "Anna", "Java", "C#", "Python", "Nalan", "Level", "Anna", "Racecar"};
+
+        List<String> list = new ArrayList<>(Arrays.asList(words));
+
+        System.out.println(list);
+
+        Iterator<String> it = list.iterator();  // iterated list of string
+
+        while(it.hasNext()){
+            String each = it.next(); // Level
+
+            String reversed = "";  // leveL
+            for(int i = each.length()-1; i >=0; i--){
+                reversed += each.charAt(i);
+            }
+
+            if(each.equalsIgnoreCase(reversed)){ // to check if the string in list is palindrome
+               it.remove();
+            }
+
+        }
+
+
+        System.out.println(list);
+
+
+    }
+
+}
