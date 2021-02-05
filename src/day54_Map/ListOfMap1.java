e11aa54ee750e9424be99592ee8cc9b09afcca8b
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-02-05 11:00:22 -0500

    day54 class

---
+++ src/day54_Map/ListOfMap1.java
@@ -0,0 +1,29 @@
+package day54_Map;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+public class ListOfMap1 {
+
+    public static void main(String[] args) {
+
+        LinkedHashMap<String, String> scrum1 = new LinkedHashMap<>();
+
+        LinkedHashMap<String, String> scrum2 = new LinkedHashMap<>();
+
+        LinkedHashMap<String, String> scrum3 = new LinkedHashMap<>();
+
+        LinkedHashMap<String, String> scrum4 = new LinkedHashMap<>();
+
+        LinkedHashMap<String, String> scrum5 = new LinkedHashMap<>();
+
+
+        List<Map<String, String>>  listOfMaps = new ArrayList<>();
+
+
+    }
+
+
+}
