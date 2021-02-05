c24e6673cbfe9a99d6775ebdb931f13a64ea8de0
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-02-05 14:24:11 -0500

    day54 class

--- src/day54_Map/ListOfMap2.java
+++ src/day54_Map/ListOfMap2.java
@@ -50,10 +50,12 @@ public class ListOfMap2 {
 
 
         System.out.println("===========================================================");
-        String[] modelOfToyota = {"Camry","Rav4","Corolla","Land Cruiser"};
-        Integer[] yearOfToyota = {2001, 2011, 2015, 2021};
-        String[] modelOfBMW = {"M1","M3","M5","X5"};
-        Integer[] yearOfBMW = {1995, 2005, 2015, 2020};
+        String[] modelOfToyota = {"Camry","Rav4","Corolla","Land Cruiser", "Venza"};
+        Integer[] yearOfToyota = {2001, 2011, 2015, 2021, 1965};
+
+        String[] modelOfBMW = {"M1","M3","M5","X5", "X6"};
+        Integer[] yearOfBMW = {1995, 2005, 2015, 2020, 2012};
+
         String[] modelOfMercedes = {"C-Class","A-Class","SLS","GLC"};
         Integer[] yearOfMercedes = {1990, 1995, 2015, 2021};
 
@@ -66,6 +68,42 @@ public class ListOfMap2 {
                             new TreeMap<>() // for BMW
                     )
         );
+
+        for(int i = 0; i < modelOfToyota.length; i++){
+            carCollection.get(1).put(modelOfToyota[i], yearOfToyota[i]);
+        }
+
+        for(int i = 0; i < modelOfBMW.length; i++){
+            carCollection.get(2).put(modelOfBMW[i], yearOfBMW[i]);
+        }
+
+        for(int i = 0; i < modelOfMercedes.length; i++){
+            carCollection.get(0).put(modelOfMercedes[i], yearOfMercedes[i]);
+        }
+
+        System.out.println(carCollection);
+
+        for (int i = 0; i < carCollection.size(); i++) {
+            Map<String, Integer> eachMap = carCollection.get(i);
+            for (Map.Entry<String, Integer> eachPair : eachMap.entrySet()) {
+                int year = eachPair.getValue();
+                if(i == 0) { // Mercedes
+                    if(year>= 1990 && year <= 1998 )
+                        System.out.println(eachPair);
+                }else if(i == 1){ // Toyota
+                    if(year >= 1960 && year <= 1970)
+                        System.out.println(eachPair);
+                }else if( i==2){ // BMW
+                    if(year >= 2011 && year <= 2015)
+                        System.out.println(eachPair);
+                }
+
+
+            }
+        }
+
+
+
 
 
         /*
