246d8e6305918b9e295e0ac74e698e10e53e0403
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-03-10 16:16:10 -0500

    Office Hour: 03/10/2021

--- src/Office_Hours/Practice_03_10_2021/ListOfMap.java
+++ src/Office_Hours/Practice_03_10_2021/ListOfMap.java
@@ -7,7 +7,7 @@ public class ListOfMap {
     public static void main(String[] args) {
         String[] namesGroup1 = {"Oliver", "Jack", "Noah", "Breanna", "Shay","James", "Conor", "Jake", "John"};
         Integer[] salariesGroup1 = {11000, 105000, 95000, 85000, 100000, 90000, 78000, 118000, 98000};
-        HashMap<String, Integer> group1 = new HashMap<>();
+        LinkedHashMap<String, Integer> group1 = new LinkedHashMap<>();
 
 
         String[] namesGroup2 = {"Harry", "Callum", "Mason", "Robert", "Keyle", "Isabella", "Mary", "Joe"};
@@ -17,10 +17,14 @@ public class ListOfMap {
 
         String[] namesGroup3 = {"Bethany", "Emily", "Susan", "Smith", "Murphy" };
         Integer[] salariesGroup4 = {125000, 100000, 115000, 95000, 85000};
-        TreeMap<String, Integer> group3 = new TreeMap<>();
+        LinkedHashMap<String, Integer> group3 = new LinkedHashMap<>();
+
 
 
         List<Map<String, Integer>> maps = new ArrayList<>();  // add all the groups into the list of map
+
+
+
 
 
     }
