888f725e2615f8c109d882c9f6c4ebe1e6eee962
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-03-10 16:36:27 -0500

    Office Hour: 03/10/2021

--- src/Office_Hours/Practice_03_10_2021/ListOfMap.java
+++ src/Office_Hours/Practice_03_10_2021/ListOfMap.java
@@ -9,20 +9,29 @@ public class ListOfMap {
         Integer[] salariesGroup1 = {11000, 105000, 95000, 85000, 100000, 90000, 78000, 118000, 98000};
         LinkedHashMap<String, Integer> group1 = new LinkedHashMap<>();
 
+        for(int i = 0; i < namesGroup1.length; i++ ){
+            group1.put( namesGroup1[i],  salariesGroup1[i] );
+        }
 
 
         String[] namesGroup2 = {"Harry", "Callum", "Mason", "Robert", "Keyle", "Isabella", "Mary", "Joe"};
         Integer[] salariesGroup2 = {88000, 94000, 104000, 108000, 98000, 105000, 85000, 100000};
         LinkedHashMap<String, Integer> group2 = new LinkedHashMap<>();
 
+        for(int i = 0; i < namesGroup2.length; i++ ){
+            group1.put( namesGroup2[i],  salariesGroup2[i] );
+        }
 
         String[] namesGroup3 = {"Bethany", "Emily", "Susan", "Smith", "Murphy" };
-        Integer[] salariesGroup4 = {125000, 100000, 115000, 95000, 85000};
+        Integer[] salariesGroup3 = {125000, 100000, 115000, 95000, 85000};
         LinkedHashMap<String, Integer> group3 = new LinkedHashMap<>();
 
+        for(int i = 0; i < namesGroup3.length; i++ ){
+            group1.put( namesGroup3[i],  salariesGroup3[i] );
+        }
 
 
-        List<Map<String, Integer>> maps = new ArrayList<>();  // add all the groups into the list of map
+        List<Map<String, Integer>>  maps = new ArrayList<>();  // add all the groups into the list of map
 
         // who has the maximum salary?
         // who has the minimum salary?
