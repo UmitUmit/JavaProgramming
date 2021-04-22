ce1c399e54d3c3d271e1b1a1506f93dbc914acf6
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-04-22 11:26:37 -0400

    Boot Camp

---
+++ src/bootCamp/MethodPractice.java
@@ -0,0 +1,41 @@
+package bootCamp;
+
+public class MethodPractice {
+
+    public static void main(String[] args) {
+
+        // print "Puzzle"   without using ;
+        if(System.out.printf("Puzzel").equals( null )){
+
+        }
+
+        while(System.out.append("Puzzel") == null){
+
+        }
+
+
+    }
+
+
+    public static void max(int a, int b){
+        if(a == b){
+            System.out.println("Equal");
+            return;
+        }
+
+        if(a > b){
+            System.out.println(a+" is greater");
+        }
+
+        if(a < b){
+            System.out.println(b+" is greater");
+        }
+
+    }
+
+
+
+
+
+
+}
